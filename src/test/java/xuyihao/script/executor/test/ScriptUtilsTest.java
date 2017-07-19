f187bf4a8db5c0ac8366ed77787afc8f66d367bf
xuyh <xuyh@uyunsoft.cn>
2017-07-19 14:53:07 +0800

    添加读取文件内容并拼接字串执行脚本程序的测试方式

--- src/test/java/xuyihao/script/executor/test/ScriptUtilsTest.java
+++ src/test/java/xuyihao/script/executor/test/ScriptUtilsTest.java
@@ -1,17 +1,20 @@
 package xuyihao.script.executor.test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import xuyihao.script.executor.util.ScriptUtils;
 
 public class ScriptUtilsTest {
-	@Test
+	@Ignore
 	public void testInvokeScriptFunctionStringStringStringMapOfStringObject() throws Exception {
 		Map<String, Object> bindings = new HashMap<>();
 		bindings.put("name", "Johnson");
@@ -22,7 +25,7 @@ public class ScriptUtilsTest {
 				bindings));
 	}
 
-	@Test
+	@Ignore
 	public void testInvokeScriptFunctionStringStringStringObjectArray() throws Exception {
 		System.out.println(ScriptUtils.invokeScriptFunction(
 				ScriptUtils.SCRIPT_GROOVY,
@@ -31,7 +34,7 @@ public class ScriptUtilsTest {
 				"Johnson", "Leo"));
 	}
 
-	@Test
+	@Ignore
 	public void testInvokeScriptFunctionStringMapOfStringObjectStringStringObjectArray() throws Exception {
 		Map<String, Object> bindings = new HashMap<>();
 		bindings.put("name", "Johnson");
@@ -43,7 +46,7 @@ public class ScriptUtilsTest {
 				"Leo"));
 	}
 
-	@Test
+	@Ignore
 	public void testInvokeFileScriptFunctionStringReaderStringMapOfStringObject() throws Exception {
 		Map<String, Object> bindings = new HashMap<>();
 		bindings.put("name", "Johnson");
@@ -55,7 +58,7 @@ public class ScriptUtilsTest {
 				bindings));
 	}
 
-	@Test
+	@Ignore
 	public void testInvokeFileScriptFunctionStringReaderStringObjectArray() throws Exception {
 		System.out.println(ScriptUtils.invokeFileScriptFunction(
 				ScriptUtils.SCRIPT_GROOVY,
@@ -65,7 +68,7 @@ public class ScriptUtilsTest {
 				"Johnson", "Leo"));
 	}
 
-	@Test
+	@Ignore
 	public void testInvokeFileScriptFunctionStringMapOfStringObjectReaderStringObjectArray() throws Exception {
 		Map<String, Object> bindings = new HashMap<>();
 		bindings.put("name", "Johnson");
@@ -76,6 +79,35 @@ public class ScriptUtilsTest {
 						new FileInputStream(new File(System.getProperty("user.dir") + "/src/test/resources/TestScript3.groovy"))),
 				"getName",
 				"Leo"));
+	}
+
+	private static final String scriptHead = "def execute(name1, name2, name3, name4){\r";
+	private static final String scriptTail = "\r}";
+
+	@Test
+	public void test() throws Exception {
+		File file = new File(System.getProperty("user.dir") + "/src/test/resources/TestScript4.groovy");
+		String fileContent = readFileIntoString(file);
+		String script = scriptHead + fileContent + scriptTail;
+		System.out.println(ScriptUtils.invokeScriptFunction(
+				ScriptUtils.SCRIPT_GROOVY, 
+				script, 
+				"execute", 
+				"Johnson ", "Leo ", "Mack ", "Mike "));
+	}
+
+	public static String readFileIntoString(File file) throws Exception {
+		String content = "";
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		char[] buffer = new char[32];
+		int length;
+		while ((length = bufferedReader.read(buffer)) > 0) {
+			for (int i = 0; i < length; i++) {
+				content += buffer[i];
+			}
+		}
+		bufferedReader.close();
+		return content;
 	}
 
 }
